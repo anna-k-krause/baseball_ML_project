663ceda7436e2846a3e24f95b8c84b4be85c4d6e
anna-k-krause <akkrause114@gmail.com>
2023-02-24 15:58:01 -0800

    Formatting Fixes 2/24

--- scripts/hw_02_main.sql
+++ scripts/hw_02_main.sql
@@ -1,38 +1,32 @@
--- Show all the available databases
--- SHOW DATABASES;
-
 -- Use baseball database for this
 USE baseball;
-
--- Show all tables
--- SHOW TABLES;
 
 -- Homework 2 Solutions
 
 -- Historic Batting Average ------
 
 -- Get totals from each
-CREATE TABLE IF NOT EXISTS hat_totals AS
+CREATE OR REPLACE TABLE hat_totals AS
 SELECT batter, SUM(Hit) AS hitSum, SUM(atBat) AS batSum
 FROM batter_counts
 GROUP BY 1
 ;
 -- calculate the batting average from each overall sum
 -- I used a case when and coalesce to solve the divide by 0 error
-CREATE TABLE IF NOT EXISTS historic_bat_avg AS
-SELECT batter, CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / batSum, 0) END AS historic_batting_average
+CREATE OR REPLACE TABLE historic_bat_avg AS
+SELECT batter, (CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / batSum, 0) END) AS historic_batting_average
 FROM hat_totals
 ORDER BY 1
 ;
 
 -- Annual Batting Average -------
 -- get each year of each game from the local_date column
-CREATE TABLE IF NOT EXISTS game_year AS
+CREATE OR REPLACE TABLE game_year AS
 SELECT game_id, DATE_FORMAT(local_date, '%Y') AS game_year
 FROM game
 ;
 -- calculate the yearly sum of hits and at bats for each batter
-CREATE TABLE IF NOT EXISTS year_hat_totals AS
+CREATE OR REPLACE TABLE year_hat_totals AS
 SELECT b.batter, g.game_year, SUM(b.Hit) AS hitSum, SUM(b.atBat) AS batSum
 FROM batter_counts b
     JOIN game_year g
@@ -41,8 +35,8 @@ GROUP BY 1, 2
 ;
 -- calculate the batting average from each overall sum for each year
 -- I used a case when and coalesce to solve the divide by 0 error
-CREATE TABLE IF NOT EXISTS yearly_bat_avg AS
-SELECT batter, game_year, CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / batSum, 0) END AS annual_batting_avg
+CREATE OR REPLACE TABLE yearly_bat_avg AS
+SELECT batter, game_year, (CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / batSum, 0) END) AS annual_batting_avg
 FROM year_hat_totals
 GROUP BY 1, 2
 ORDER BY 1
@@ -50,7 +44,7 @@ ORDER BY 1
 
 -- Last 100 Days Rolling Average ------
 -- Get all dates
-CREATE TABLE IF NOT EXISTS all_game_dates AS
+CREATE OR REPLACE TABLE all_game_dates AS
 SELECT b.batter, b.game_id, g.local_date, b.Hit, b.atBat
 FROM batter_counts b
     JOIN game g
@@ -65,7 +59,7 @@ ALTER TABLE all_game_dates ADD PRIMARY KEY (batter, game_id), ADD INDEX batter_i
 -- I used a self join and a between statement to grab the last 100 days
 -- used coalesce to convert null values to zero for the sum
 -- source : https://stackoverflow.com/questions/19299039/12-month-moving-average-by-person-date
-CREATE TABLE IF NOT EXISTS last_100_games AS
+CREATE OR REPLACE TABLE last_100_dates AS
 SELECT a.batter, a.local_date, COALESCE(d.hit, 0) AS joined_hit, COALESCE(d.atBat, 0) AS joined_atBat
 FROM all_game_dates a
     LEFT JOIN all_game_dates d
@@ -74,16 +68,16 @@ FROM all_game_dates a
             AND DATE_ADD(a.local_date, INTERVAL - 1 DAY)
 ;
 -- getting at bat and hit sums for each 100 day span
-CREATE TABLE IF NOT EXISTS last_100_hat_totals AS
+CREATE OR REPLACE TABLE last_100_hat_totals AS
 SELECT batter, local_date, SUM(joined_hit) AS hitSum, SUM(joined_atBat) AS batSum
-FROM last_100_games
+FROM last_100_dates
 GROUP BY 1, 2
 ;
 -- Create final avg for each 100 day span for each batter for each game
 -- I used a case when and coalesce to solve the divide by 0 error
-CREATE TABLE IF NOT EXISTS last_100_games_rolling_avg AS
-SELECT batter, local_date, CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / batSum, 0) END AS rolling_avg
+CREATE OR REPLACE TABLE last_100_games_rolling_avg AS
+SELECT batter, local_date, (CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / batSum, 0) END) AS rolling_avg
 FROM last_100_hat_totals
 ORDER BY 1, 2
 ;
--- This ran on my computer in 3 minutes 38 seconds
+-- This ran on my computer in 2 minutes 6 seconds
