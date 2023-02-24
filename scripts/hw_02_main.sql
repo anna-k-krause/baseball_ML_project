df49de4579108a20aeb04e5cb05e3facb0e81ecf
anna-k-krause <akkrause114@gmail.com>
2023-02-24 00:35:43 -0800

    Commit 2/24/23

--- scripts/hw_02_main.sql
+++ scripts/hw_02_main.sql
@@ -49,36 +49,26 @@ ORDER BY 1
 ;
 
 -- Last 100 Days Rolling Average ------
-
--- Setup Indexes
--- I added indexes for each table I JOINED on or did an ORDER BY on
-ALTER TABLE game
-ADD INDEX local_date_index(local_date)
-;
-
-ALTER TABLE batter_counts
-ADD INDEX batter_index(batter)
-;
--- source : https://stackoverflow.com/questions/5277597/what-column-to-index-on-and-making-table-search-fast
-
--- Queries
 -- Get all dates
-CREATE TABLE IF NOT EXISTS all_dates AS
-SELECT b.batter, g.local_date, b.Hit, b.atBat
+CREATE TABLE IF NOT EXISTS all_game_dates AS
+SELECT b.batter, b.game_id, g.local_date, b.Hit, b.atBat
 FROM batter_counts b
     JOIN game g
         ON b.game_id = g.game_id
 ;
--- WHERE batter = '110029'
--- I pasted test values above
+-- Manage Primary Keys and Add Indexes
+ALTER TABLE all_game_dates ADD PRIMARY KEY (batter, game_id), ADD INDEX batter_index_test(batter);
 
+-- source : https://stackoverflow.com/questions/5277597/what-column-to-index-on-and-making-table-search-fast
+
+-- Get the last 100 games for each date and batter with the needed stats
 -- I used a self join and a between statement to grab the last 100 days
 -- used coalesce to convert null values to zero for the sum
 -- source : https://stackoverflow.com/questions/19299039/12-month-moving-average-by-person-date
 CREATE TABLE IF NOT EXISTS last_100_games AS
 SELECT a.batter, a.local_date, COALESCE(d.hit, 0) AS joined_hit, COALESCE(d.atBat, 0) AS joined_atBat
-FROM all_dates a
-    LEFT JOIN all_dates d
+FROM all_game_dates a
+    LEFT JOIN all_game_dates d
         ON d.batter = a.batter
             AND d.local_date BETWEEN DATE_ADD(a.local_date, INTERVAL - 101 DAY)
             AND DATE_ADD(a.local_date, INTERVAL - 1 DAY)
@@ -96,101 +86,4 @@ SELECT batter, local_date, CASE WHEN batSum = 0 THEN 0 ELSE COALESCE(hitSum / ba
 FROM last_100_hat_totals
 ORDER BY 1, 2
 ;
--- this took 21 minutes to run on my computer
-
--- old work
-/*
-WITH hat_totals AS (
-    SELECT batter, SUM(Hit) AS hitSum, SUM(atBat) AS batSum
-    FROM batter_counts
-    GROUP BY 1
-)
--- calculate the batting average from each overall sum
-
-SELECT batter, COALESCE(hitSum / batSum, 0) AS historic_batting_average
-FROM hat_totals
-ORDER BY 1
-;
-
--- Annual Batting Average
-WITH game_year AS (
-    SELECT game_id, DATE_FORMAT(local_date, '%Y') AS game_year
-    FROM game
-)
-,
--- calculate the yearly sum of hits and at bats for each batter
-year_hat_totals AS (
-    SELECT b.batter, g.game_year, SUM(b.Hit) AS hitSum, SUM(b.atBat) AS batSum
-    FROM batter_counts b
-        JOIN game_year g
-            ON b.game_id = g.game_id
-    GROUP BY 1, 2
-)
--- calculate the batting average from each overall sum
-SELECT batter, game_year, COALESCE(hitSum / batSum, 0) AS annual_batting_avg
-FROM year_hat_totals
-GROUP BY 1, 2
-ORDER BY 1
-;
-
--- source : https://stackoverflow.com/questions/26322398/trunc-date-field-in-mysql-like-oracle
-
-
--- Last 100 Days Rolling Average
-CREATE TABLE game_2 LIKE game
-;
-INSERT INTO game_2 SELECT * FROM game
-;
-ALTER TABLE game_2
-ADD INDEX local_date_index(local_date)
-;
-
-CREATE TABLE batter_counts_2 LIKE batter_counts
-;
-
-INSERT INTO batter_counts_2 SELECT * FROM batter_counts
-;
-ALTER TABLE batter_counts_2
-ADD INDEX batter_index(batter)
-;
-
--- source : https://popsql.com/learn-sql/mysql/how-to-duplicate-a-table-in-mysql
-
--- Last 100 Days Rolling Average
-WITH all_dates AS (
-    SELECT b.batter, g.local_date, b.Hit, b.atBat
-    FROM batter_counts_2 b
-        JOIN game_2 g
-            ON b.game_id = g.game_id
-    WHERE batter = '110029'
-)
-,
-last_100_games AS (
-    SELECT a.batter, a.local_date, COALESCE(d.hit, 0) AS joined_hit, COALESCE(d.atBat, 0) AS joined_atBat
-    FROM all_dates a
-        LEFT JOIN all_dates d
-            ON d.batter = a.batter
-                AND d.local_date BETWEEN DATE_ADD(a.local_date, INTERVAL - 100 DAY)
-                AND DATE_ADD(a.local_date, INTERVAL - 1 DAY)
-)
-,
-last_100_hat_totals AS (
-    SELECT batter, local_date, SUM(joined_hit) AS hitSum, SUM(joined_atBat) AS batSum
-    FROM last_100_games
-    GROUP BY 1, 2
-)
-SELECT batter, local_date, COALESCE(hitSum / batSum, 0) AS rolling_avg
-FROM last_100_hat_totals
-ORDER BY 1, 2
-;
-
--- this took 21 minutes to run on my computer
--- source : https://stackoverflow.com/questions/5277597/what-column-to-index-on-and-making-table-search-fast
--- source : https://stackoverflow.com/questions/19299039/12-month-moving-average-by-person-date
-
-
--- 116338
--- 120074
--- 110029
--- 110683
-*/
+-- This ran on my computer in 3 minutes 38 seconds
